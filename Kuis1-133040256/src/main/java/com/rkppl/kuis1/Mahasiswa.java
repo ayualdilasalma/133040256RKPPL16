fd269a94c3736876480aeeafcfa7c24fba80b81f
srs"249 <syauqi.133040249@mail.unpas.ac.id>
2016-04-22 13:42:02 +0700

    Initial Commit

---
+++ Kuis1-133040256/src/main/java/com/rkppl/kuis1/Mahasiswa.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rkppl.kuis1;
+
+/**
+ *
+ * @author SB604-09
+ */
+public class Mahasiswa {
+    private String nama, nrp;
+    private int umur;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNrp() {
+        return nrp;
+    }
+
+    public void setNrp(String nrp) {
+        this.nrp = nrp;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+    
+}
